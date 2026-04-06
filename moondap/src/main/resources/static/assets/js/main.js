a9a957b231442cb68ce865e816eb6162f9034401
koogc0724@gmail.com <koogc0724@gmail.com>
2026-04-06 21:54:22 +0900

    index.html 태그명 변경 2

--- moondap/src/main/resources/static/assets/js/main.js
+++ moondap/src/main/resources/static/assets/js/main.js
@@ -119,24 +119,24 @@
   /**
    * Init isotope layout and filters
    */
-  document.querySelectorAll('.isotope-layout').forEach(function(isotopeItem) {
+  document.querySelectorAll('.game-gallery-layout').forEach(function(isotopeItem) {
     let layout = isotopeItem.getAttribute('data-layout') ?? 'masonry';
     let filter = isotopeItem.getAttribute('data-default-filter') ?? '*';
     let sort = isotopeItem.getAttribute('data-sort') ?? 'original-order';
 
     let initIsotope;
-    imagesLoaded(isotopeItem.querySelector('.isotope-container'), function() {
-      initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
-        itemSelector: '.isotope-item',
+    imagesLoaded(isotopeItem.querySelector('.game-gallery-container'), function() {
+      initIsotope = new Isotope(isotopeItem.querySelector('.game-gallery-container'), {
+        itemSelector: '.game-item',
         layoutMode: layout,
         filter: filter,
         sortBy: sort
       });
     });
 
-    isotopeItem.querySelectorAll('.isotope-filters li').forEach(function(filters) {
+    isotopeItem.querySelectorAll('.game-gallery-filters li').forEach(function(filters) {
       filters.addEventListener('click', function() {
-        isotopeItem.querySelector('.isotope-filters .filter-active').classList.remove('filter-active');
+        isotopeItem.querySelector('.game-gallery-filters .filter-active').classList.remove('filter-active');
         this.classList.add('filter-active');
         initIsotope.arrange({
           filter: this.getAttribute('data-filter')
